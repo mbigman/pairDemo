eff092d0d9426c02e35cd13ca618d524b4fbc826
Maxwell Bigman <mbigman@Maxwells-MBP.attlocal.net>
2020-03-18 12:08:16 -0700

    copied in basic code

---
+++ pair-app/src/routes.js
@@ -0,0 +1,20 @@
+// src/index.js
+
+/* eslint-disable import/default */
+
+import 'babel-polyfill' ;
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+    <Router routes={routes} history={browserHistory} />
+  </Provider>,
+ document.getElementById('main')
+);
